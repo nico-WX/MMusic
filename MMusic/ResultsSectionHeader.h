7bf62087c93db373492f6437cd7f3e8529b1447f
pan <nico_WX@163.com>
2018-04-10 22:12:23 +0800

    搜索分页加载更多内容

---
+++ MMusic/ResultsSectionHeader.h
@@ -0,0 +1,13 @@
+//
+//  ResultsSectionView.h
+//  MMusic
+//
+//  Created by Magician on 2018/4/10.
+//  Copyright © 2018年 com.😈. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ResultsSectionHeader : UITableViewHeaderFooterView
+
+@end
---
+++ MMusic/ResultsSectionHeader.m
@@ -0,0 +1,22 @@
+//
+//  ResultsSectionView.m
+//  MMusic
+//
+//  Created by Magician on 2018/4/10.
+//  Copyright © 2018年 com.😈. All rights reserved.
+//
+
+#import "ResultsSectionHeader.h"
+
+@implementation ResultsSectionHeader
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+  > ^ >< <> >> <<
+}
+*/
+
+@end
