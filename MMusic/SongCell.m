0edfd449e08adfc13fe6653ff0e08d1a0a2795a4
pan <nico_WX@163.com>
2018-12-01 21:28:47 +0800

    修复song 选中动画

--- MMusic/SongCell.m
+++ MMusic/SongCell.m
@@ -161,48 +161,59 @@
     [self setNeedsDisplay];
 }
 
-- (UITableViewCellSelectionStyle)selectionStyle{
-    return UITableViewCellSelectionStyleGray;
-}
+//- (UITableViewCellSelectionStyle)selectionStyle{
+//    return UITableViewCellSelectionStyleGray;
+//}
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated{
     [super setSelected:selected animated:animated];
 
     if (animated && selected) {
 
-        UIGraphicsBeginImageContext(self.bounds.size);
+        UIImage *image = [self imageWithCurrentContext];
 
-        CGContextRef context = UIGraphicsGetCurrentContext();
-        [self.layer renderInContext:context];
-        UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
-        UIGraphicsEndImageContext();
+        UIImageView *imageView = [[UIImageView alloc] initWithFrame:self.bounds];
+        [imageView setImage:image];
+        [imageView.layer setShadowOffset:CGSizeMake(4, 6)];
+        [imageView.layer setShadowOpacity:1];
+        [imageView.layer setShadowColor:UIColor.darkGrayColor.CGColor];
 
-        UIImageView *imageView = [[UIImageView alloc] initWithImage:image];
-        [imageView setFrame:self.bounds];
         [self.contentView addSubview:imageView];
 
         __block CGRect frame = self.bounds;
         frame.origin.y -= 8;
         frame.origin.x += 8;
-        [UIView animateWithDuration:0.3 animations:^{
+
+        [UIView animateWithDuration:0.2 animations:^{
+            //禁用, 防止不断点击
+            [self setUserInteractionEnabled:NO];
             [imageView setFrame:frame];
         } completion:^(BOOL finished) {
             if (finished) {
                 frame.origin.x = CGRectGetMaxX([UIScreen mainScreen].bounds);
-                [UIView animateWithDuration:2 animations:^{
+                [UIView animateWithDuration:0.8 animations:^{
                     [imageView setFrame:frame];
                 } completion:^(BOOL finished) {
+                    [self setUserInteractionEnabled:YES];
                     [imageView removeFromSuperview];
                 }];
             }
-
         }];
-
-
-
-
-
     }
+}
+
+//当前图层 绘制成图片
+- (UIImage*)imageWithCurrentContext{
+
+    UIGraphicsBeginImageContextWithOptions(self.bounds.size, 1, [[UIScreen mainScreen] scale]);
+   // UIGraphicsBeginImageContext(size);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+
+    [self.layer renderInContext:context];
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+
+    UIGraphicsEndImageContext();
+    return image;
 }
 
 @end
